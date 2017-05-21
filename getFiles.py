bb5c39359958e4a1e5119f50853f88c2cb090bf8
Aditya Sharma <adityashrm21@gmail.com>
2017-05-21 19:30:41 +0530

    Update getFiles.py

--- getFiles.py
+++ getFiles.py
@@ -3,7 +3,7 @@ from shutil import copyfile
 
 dst='destination_dir'
 os.chdir('source_dir')
-for file in glob.glob("**/*.py", recursive=True):
+for file in glob.glob("**/*.extension", recursive=True):
     
     filename=file.split('/')[-1]
     copyfile(file, os.path.join(dst, filename))
